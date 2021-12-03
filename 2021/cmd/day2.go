adfd18f60d6570427d2aaada119da815d40b1cda
Riley Chase <riley.chase@rileychase.net>
2021-12-03 23:00:29 +1030

    Add day 2, part 2 solution

--- 2021/cmd/day2.go
+++ 2021/cmd/day2.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"2021.advent-of-code.rileychase.net/internal/utils"
 )
@@ -9,23 +10,35 @@ import (
 type submarine struct {
 	horizontal int
 	depth      int
+	aim        int
+	aimEnabled bool
 }
 
 func (s *submarine) Forward(x int) {
+	if s.aimEnabled {
+		s.depth += s.aim * x
+	}
 	s.horizontal += x
 }
 
 func (s *submarine) Down(x int) {
-	// Go down -> increase depth
-	s.depth += x
+	if s.aimEnabled {
+		s.aim += x
+	} else {
+		s.depth += x
+	}
 }
 
 func (s *submarine) Up(x int) {
-	s.depth -= x
+	if s.aimEnabled {
+		s.aim -= x
+	} else {
+		s.depth -= x
+	}
 }
 
 func day2Part1(commands []utils.Command) (int, error) {
-	var sub submarine
+	sub := submarine{aimEnabled: false}
 
 	for _, command := range commands {
 		switch command.Direction {
@@ -38,6 +51,26 @@ func day2Part1(commands []utils.Command) (int, error) {
 		default:
 			return 0, fmt.Errorf("unknown submarine command %+v", command)
 		}
+	}
+
+	return sub.depth * sub.horizontal, nil
+}
+
+func day2Part2(commands []utils.Command) (int, error) {
+	sub := submarine{aimEnabled: true}
+
+	for _, command := range commands {
+		switch command.Direction {
+		case "forward":
+			sub.Forward(command.Magnitude)
+		case "down":
+			sub.Down(command.Magnitude)
+		case "up":
+			sub.Up(command.Magnitude)
+		default:
+			return 0, fmt.Errorf("unknown submarine command %+v", command)
+		}
+		log.Printf("%+v -> %+v", command, sub)
 	}
 
 	return sub.depth * sub.horizontal, nil
