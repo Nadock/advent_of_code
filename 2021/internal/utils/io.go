003a08ad0e2522db0e5dc5f1a26ddb90aea87ca4
Riley Chase <riley.chase@rileychase.net>
2021-12-03 23:30:01 +1030

    Add util function to read day 3 input files

--- 2021/internal/utils/io.go
+++ 2021/internal/utils/io.go
@@ -63,3 +63,12 @@ func ReadCommandFile(path string) ([]Command, error) {
 
 	return commands, nil
 }
+
+func ReadBinaryDiagnosticFile(path string) ([]string, error) {
+	input, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return strings.Split(string(input), "\n"), nil
+}
