2e868cd137bc9f580c5079dab8d47e98f561e2a2
Riley Chase <riley.chase@rileychase.net>
2021-12-03 22:48:02 +1030

    Add day 2, part 1 tests

---
+++ 2021/cmd/day2_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+
+	"2021.advent-of-code.rileychase.net/internal/utils"
+)
+
+func TestDay2Part1Example(t *testing.T) {
+	commands, err := utils.ReadCommandFile("../inputs/day2/example_input.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := 150
+	answer, err := day2Part1(commands)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if answer != expected {
+		t.Errorf("expected answer of %d, got %d", expected, answer)
+	}
+}
+
+func TestDay2Part1Test(t *testing.T) {
+	commands, err := utils.ReadCommandFile("../inputs/day2/test_input.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := 1693300
+	answer, err := day2Part1(commands)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if answer != expected {
+		t.Errorf("expected answer of %d, got %d", expected, answer)
+	}
+}
