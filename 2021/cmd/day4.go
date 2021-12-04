fd90089a42790a7444fd66e58a2df5f247f472a7
Riley Chase <riley.chase@rileychase.net>
2021-12-04 16:32:31 +1030

    Add day 4, part 2 solution

--- 2021/cmd/day4.go
+++ 2021/cmd/day4.go
@@ -12,7 +12,7 @@ func day4part1(drawnNumbers []string, boards []*utils.Board) (int, error) {
 	// log.Printf("%v, %+v", drawnNumbers, boards)
 	// log.Printf("")
 
-	winNumber, winBoard, err := simulateBingoGame(drawnNumbers, boards)
+	winNumber, winBoard, err := simulateWinFirstBingoGame(drawnNumbers, boards)
 	if err != nil {
 		return 0, err
 	}
@@ -27,7 +27,24 @@ func day4part1(drawnNumbers []string, boards []*utils.Board) (int, error) {
 	return score * winNumber, nil
 }
 
-func simulateBingoGame(drawnNumbers []string, boards []*utils.Board) (int, *utils.Board, error) {
+func day4part2(drawnNumbers []string, boards []*utils.Board) (int, error) {
+
+	winNumber, winBoard, err := simulateWinLastBingoGame(drawnNumbers, boards)
+	if err != nil {
+		return 0, err
+	}
+	log.Printf("last winning number: %d, last winning board: %+v", winNumber, winBoard)
+
+	score, err := winBoard.Score()
+	if err != nil {
+		return 0, err
+	}
+	log.Printf("last winning board score: %d", score)
+
+	return score * winNumber, nil
+}
+
+func simulateWinFirstBingoGame(drawnNumbers []string, boards []*utils.Board) (int, *utils.Board, error) {
 	for _, draw := range drawnNumbers {
 		// log.Printf("draw %s", draw)
 		for _, board := range boards {
@@ -44,4 +61,41 @@ func simulateBingoGame(drawnNumbers []string, boards []*utils.Board) (int, *util
 	}
 
 	return 0, nil, fmt.Errorf("no winner after drawing all numbers")
+}
+
+func simulateWinLastBingoGame(drawnNumbers []string, boards []*utils.Board) (int, *utils.Board, error) {
+	boardsHaveWon := make([]bool, len(boards))
+
+	for _, draw := range drawnNumbers {
+		// log.Printf("drawing %s", draw)
+		for _, board := range boards {
+			board.Draw(draw)
+		}
+
+		for idx, board := range boards {
+			if !boardsHaveWon[idx] {
+				if board.IsWinner() {
+					// log.Printf("board %d has won", idx)
+
+					winnersCount := 0
+					for _, hasWon := range boardsHaveWon {
+						if hasWon {
+							winnersCount++
+						}
+					}
+
+					if winnersCount == len(boardsHaveWon)-1 {
+						// log.Printf("board %d is last to win", idx)
+						winningDraw, err := strconv.Atoi(draw)
+						return winningDraw, board, err
+					}
+
+					boardsHaveWon[idx] = true
+				}
+			}
+
+		}
+	}
+
+	return 0, nil, fmt.Errorf("no last winner after drawing all numbers")
 }
