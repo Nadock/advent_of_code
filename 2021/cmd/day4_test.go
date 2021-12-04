0d08631587f895f4ef9230d908fb9b3d500f9674
Riley Chase <riley.chase@rileychase.net>
2021-12-04 16:32:35 +1030

    Add day 4, part 2 tests

--- 2021/cmd/day4_test.go
+++ 2021/cmd/day4_test.go
@@ -39,3 +39,37 @@ func TestDay4Part1Test(t *testing.T) {
 		t.Errorf("expected answer of %d, got %d", expected, answer)
 	}
 }
+
+func TestDay4Part2Example(t *testing.T) {
+	drawnNumbers, boards, err := utils.ReadBingoGameFile("../inputs/day4/example.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := 1924
+	answer, err := day4part2(drawnNumbers, boards)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if answer != expected {
+		t.Errorf("expected answer of %d, got %d", expected, answer)
+	}
+}
+
+func TestDay4Part2Test(t *testing.T) {
+	drawnNumbers, boards, err := utils.ReadBingoGameFile("../inputs/day4/test.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := 8468
+	answer, err := day4part2(drawnNumbers, boards)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if answer != expected {
+		t.Errorf("expected answer of %d, got %d", expected, answer)
+	}
+}
