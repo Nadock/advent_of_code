e39571430a7075b2bae6367ddbba09d5c9b4c54b
Riley Chase <riley.chase@rileychase.net>
2021-12-05 20:15:25 +1030

    Add day 5, part 2 tests

--- 2021/cmd/day5_test.go
+++ 2021/cmd/day5_test.go
@@ -12,7 +12,7 @@ func TestDay5Part1Example(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	expected := 50
+	expected := 5
 	answer, err := day5part1(lines)
 	if err != nil {
 		t.Fatal(err)
@@ -31,6 +31,40 @@ func TestDay5Part1Test(t *testing.T) {
 
 	expected := 4728
 	answer, err := day5part1(lines)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if answer != expected {
+		t.Errorf("expected answer of %d, got %d", expected, answer)
+	}
+}
+
+func TestDay5Part2Example(t *testing.T) {
+	lines, err := utils.ReadHydrothermalVentsMapFile("../inputs/day5/example.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := 12
+	answer, err := day5part2(lines)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if answer != expected {
+		t.Errorf("expected answer of %d, got %d", expected, answer)
+	}
+}
+
+func TestDay5Part2Test(t *testing.T) {
+	lines, err := utils.ReadHydrothermalVentsMapFile("../inputs/day5/test.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := 17717
+	answer, err := day5part2(lines)
 	if err != nil {
 		t.Fatal(err)
 	}
