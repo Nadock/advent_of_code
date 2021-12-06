be0a5ec6b85d03d669e6656203ea2d830adfb476
Riley Chase <riley.chase@rileychase.net>
2021-12-06 20:00:17 +1030

    Add day 6, part 1 solution

---
+++ 2021/day6/day6.go
@@ -0,0 +1,49 @@
+package day6
+
+import (
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func ReadLanternFishAgeFile(path string) ([]int, error) {
+	input, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var ages []int
+
+	nums := strings.Split(strings.TrimSpace(string(input)), ",")
+	for _, n := range nums {
+		age, err := strconv.Atoi(strings.TrimSpace(n))
+		if err != nil {
+			return nil, err
+		}
+		ages = append(ages, age)
+	}
+
+	return ages, nil
+}
+
+func day6part1(ages []int) (int, error) {
+	log.Printf("initial state: %v", ages)
+	for day := 0; day < 80; day++ {
+		var newFish []int
+
+		for i, fish := range ages {
+			if fish == 0 {
+				newFish = append(newFish, 8)
+				ages[i] = 6
+			} else {
+				ages[i]--
+			}
+		}
+
+		ages = append(ages, newFish...)
+		log.Printf("after %d days: %v", day, ages)
+	}
+
+	return len(ages), nil
+}
