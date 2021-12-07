de010d35578dd316a28763ab5eeb52f27dafb41a
Riley Chase <riley.chase@rileychase.net>
2021-12-07 22:51:18 +1030

    Update day 6 to a Go implementation of solution

--- 2021/day6/day6.go
+++ 2021/day6/day6.go
@@ -1,51 +1,39 @@
 package day6
 
-import (
-	"io/ioutil"
-	"log"
-	"strconv"
-	"strings"
-)
-
-func ReadLanternFishAgeFile(path string) ([]int, error) {
-	input, err := ioutil.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-
-	var ages []int
-
-	nums := strings.Split(strings.TrimSpace(string(input)), ",")
-	for _, n := range nums {
-		age, err := strconv.Atoi(strings.TrimSpace(n))
-		if err != nil {
-			return nil, err
-		}
-		ages = append(ages, age)
-	}
-
-	return ages, nil
+func day6part1(ages []int) int {
+	fc := newFishCounter()
+	return fc.countForDays(ages, 80)
 }
 
-func day6part1(ages []int) int {
-	// log.Printf("initial state: %v (%d)", ages, len(ages))
-	// log.Printf("day,ages")
-	for day := 0; day < 80; day++ {
-		var newFish []int
+func day6part2(ages []int) int {
+	fc := newFishCounter()
+	return fc.countForDays(ages, 256)
+}
 
-		for i, fish := range ages {
-			if fish == 0 {
-				newFish = append(newFish, 8)
-				ages[i] = 6
-			} else {
-				ages[i]--
-			}
-		}
+type fishCounter struct {
+	cache map[int]int
+}
 
-		ages = append(ages, newFish...)
-		// log.Printf("after %d days: %v", day, ages)
-		// log.Printf("%d,%d", day+1, len(ages))
+func newFishCounter() *fishCounter {
+	return &fishCounter{cache: make(map[int]int)}
+}
+
+func (fc *fishCounter) countForDays(ages []int, day int) int {
+	total := 0
+	for _, fish := range ages {
+		total += fc.countFish(day - 1 - fish)
 	}
+	return total
+}
 
-	return len(ages)
+func (fc *fishCounter) countFish(life int) int {
+	if life < 0 {
+		return 1
+	}
+	if value, found := fc.cache[life]; found {
+		return value
+	}
+	result := fc.countFish(life-7) + fc.countFish(life-9)
+	fc.cache[life] = result
+	return result
 }
--- 2021/day6/day6_test.go
+++ 2021/day6/day6_test.go
@@ -1,9 +1,13 @@
 package day6
 
-import "testing"
+import (
+	"testing"
+
+	"2021.advent-of-code.rileychase.net/internal"
+)
 
 func TestDay6Part1Example(t *testing.T) {
-	ages, err := ReadLanternFishAgeFile("./example.txt")
+	ages, err := internal.ReadIntValues("./example.txt")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -17,13 +21,41 @@ func TestDay6Part1Example(t *testing.T) {
 }
 
 func TestDay6Part1Test(t *testing.T) {
-	ages, err := ReadLanternFishAgeFile("./test.txt")
+	ages, err := internal.ReadIntValues("./test.txt")
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	expected := 372984
 	answer := day6part1(ages)
+
+	if answer != expected {
+		t.Errorf("expected answer of %d, got %d", expected, answer)
+	}
+}
+
+func TestDay6Part2Example(t *testing.T) {
+	ages, err := internal.ReadIntValues("./example.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := 26984457539
+	answer := day6part2(ages)
+
+	if answer != expected {
+		t.Errorf("expected answer of %d, got %d", expected, answer)
+	}
+}
+
+func TestDay6Part2Test(t *testing.T) {
+	ages, err := internal.ReadIntValues("./test.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := 1681503251694
+	answer := day6part2(ages)
 
 	if answer != expected {
 		t.Errorf("expected answer of %d, got %d", expected, answer)
