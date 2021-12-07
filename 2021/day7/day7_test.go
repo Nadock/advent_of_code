62abc2d1a8b8f69cf87114a5cd10058dd9e1fee0
Riley Chase <riley.chase@rileychase.net>
2021-12-07 17:20:26 +1030

    Add day 7, part 2 test

--- 2021/day7/day7_test.go
+++ 2021/day7/day7_test.go
@@ -32,36 +32,30 @@ func TestDay7Part1Test(t *testing.T) {
 	}
 }
 
-// func TestDay7Part2Example(t *testing.T) {
-// 	positions, err := ReadCrabPositionsFile("./example.txt")
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+func TestDay7Part2Example(t *testing.T) {
+	positions, err := ReadCrabPositionsFile("./example.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	expected := 12
-// 	answer, err := day7part2(positions)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+	expected := 168
+	answer := day7part2(positions)
 
-// 	if answer != expected {
-// 		t.Errorf("expected answer of %d, got %d", expected, answer)
-// 	}
-// }
+	if answer != expected {
+		t.Errorf("expected answer of %d, got %d", expected, answer)
+	}
+}
 
-// func TestDay7Part2Test(t *testing.T) {
-// 	positions, err := ReadCrabPositionsFile("./test.txt")
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+func TestDay7Part2Test(t *testing.T) {
+	positions, err := ReadCrabPositionsFile("./test.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	expected := 17717
-// 	answer, err := day7part2(positions)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+	expected := 98039527
+	answer := day7part2(positions)
 
-// 	if answer != expected {
-// 		t.Errorf("expected answer of %d, got %d", expected, answer)
-// 	}
-// }
+	if answer != expected {
+		t.Errorf("expected answer of %d, got %d", expected, answer)
+	}
+}
