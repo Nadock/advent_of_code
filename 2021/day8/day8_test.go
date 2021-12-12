850d4dbde3396c5fa77fe8d1061502ab60f312ee
Riley Chase <riley.chase@rileychase.net>
2021-12-12 14:00:11 +1030

    Add day 8, part 2 tests

--- 2021/day8/day8_test.go
+++ 2021/day8/day8_test.go
@@ -26,30 +26,26 @@ func TestDay7Part1Test(t *testing.T) {
 	}
 }
 
-// func TestDay7Part2Example(t *testing.T) {
-// 	positions, err := internal.ReadIntValues("./example.txt")
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+func TestDay7Part2Example(t *testing.T) {
+	expected := 26
+	answer, err := day8part2("./example.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	expected := 168
-// 	answer := day7part2(positions)
+	if answer != expected {
+		t.Errorf("expected answer of %d, got %d", expected, answer)
+	}
+}
 
-// 	if answer != expected {
-// 		t.Errorf("expected answer of %d, got %d", expected, answer)
-// 	}
-// }
+func TestDay7Part2Test(t *testing.T) {
+	expected := 262
+	answer, err := day8part2("./test.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// func TestDay7Part2Test(t *testing.T) {
-// 	positions, err := internal.ReadIntValues("./test.txt")
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// 	expected := 98039527
-// 	answer := day7part2(positions)
-
-// 	if answer != expected {
-// 		t.Errorf("expected answer of %d, got %d", expected, answer)
-// 	}
-// }
+	if answer != expected {
+		t.Errorf("expected answer of %d, got %d", expected, answer)
+	}
+}
