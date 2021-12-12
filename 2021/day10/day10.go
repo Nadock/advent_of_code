5555efd4a9ba4d91317b9ad0eabcb11100e8f570
Riley Chase <riley.chase@rileychase.net>
2021-12-12 22:12:22 +1030

    Add day 10, part 2 solution

--- 2021/day10/day10.go
+++ 2021/day10/day10.go
@@ -2,6 +2,7 @@ package day10
 
 import (
 	"log"
+	"sort"
 	"strings"
 
 	"2021.advent-of-code.rileychase.net/internal"
@@ -60,7 +61,6 @@ func findErrors(line string) string {
 			}
 
 			if c != e {
-				log.Printf("expected %s, but found %s instead", e, c)
 				return c
 			}
 		}
@@ -75,7 +75,77 @@ func (s *stack) Push(v string) {
 }
 
 func (s *stack) Pop() string {
+	if s.Empty() {
+		return ""
+	}
 	v := (*s)[len(*s)-1]
 	*s = (*s)[:len(*s)-1]
 	return v
+}
+
+func (s *stack) Empty() bool {
+	return len(*s) == 0
+}
+
+func day10part2(path string) (int, error) {
+	lines, err := internal.ReadLines(path)
+	if err != nil {
+		return 0, err
+	}
+
+	var scores []int
+	for _, line := range lines {
+		if i := findErrors(line); i == "" {
+			fixes := repairLine(line)
+			score := 0
+			for _, c := range fixes {
+				score *= 5
+				switch c {
+				case ")":
+					score += 1
+				case "]":
+					score += 2
+				case "}":
+					score += 3
+				case ">":
+					score += 4
+				}
+			}
+			log.Printf("fix line=%s by adding %s — score %d", line, strings.Join(fixes, ""), score)
+			scores = append(scores, score)
+		}
+	}
+
+	sort.Ints(scores)
+	log.Printf("scores: %v, mid: %d, val: %d", scores, (len(scores) / 2), scores[(len(scores)/2)])
+
+	return scores[(len(scores) / 2)], nil
+}
+
+func repairLine(line string) []string {
+	var s stack
+	for _, c := range strings.Split(line, "") {
+		if c == "(" || c == "[" || c == "{" || c == "<" {
+			s.Push(c)
+		} else {
+			s.Pop()
+		}
+	}
+
+	var repair []string
+	for !s.Empty() {
+		next := s.Pop()
+		switch next {
+		case "(":
+			repair = append(repair, ")")
+		case "[":
+			repair = append(repair, "]")
+		case "{":
+			repair = append(repair, "}")
+		case "<":
+			repair = append(repair, ">")
+		}
+	}
+
+	return repair
 }
