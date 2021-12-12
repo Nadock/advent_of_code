05a634c3af73e6c02082bcd1967332842f4af29f
Riley Chase <riley.chase@rileychase.net>
2021-12-12 14:27:09 +1030

    Finish day 8 unit tests

--- 2021/day8/day8_test.go
+++ 2021/day8/day8_test.go
@@ -2,7 +2,7 @@ package day8
 
 import "testing"
 
-func TestDay7Part1Example(t *testing.T) {
+func TestDay8Part1Example(t *testing.T) {
 	expected := 26
 	answer, err := day8part1("./example.txt")
 	if err != nil {
@@ -14,7 +14,7 @@ func TestDay7Part1Example(t *testing.T) {
 	}
 }
 
-func TestDay7Part1Test(t *testing.T) {
+func TestDay8Part1Test(t *testing.T) {
 	expected := 261
 	answer, err := day8part1("./test.txt")
 	if err != nil {
@@ -26,8 +26,8 @@ func TestDay7Part1Test(t *testing.T) {
 	}
 }
 
-func TestDay7Part2Example(t *testing.T) {
-	expected := 26
+func TestDay8Part2Example(t *testing.T) {
+	expected := 61229
 	answer, err := day8part2("./example.txt")
 	if err != nil {
 		t.Fatal(err)
@@ -38,8 +38,8 @@ func TestDay7Part2Example(t *testing.T) {
 	}
 }
 
-func TestDay7Part2Test(t *testing.T) {
-	expected := 262
+func TestDay8Part2Test(t *testing.T) {
+	expected := 987553
 	answer, err := day8part2("./test.txt")
 	if err != nil {
 		t.Fatal(err)
