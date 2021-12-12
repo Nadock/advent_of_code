8fe9ea473d3470f3145c0feaec8a51c02c68093d
Riley Chase <riley.chase@rileychase.net>
2021-12-12 22:12:26 +1030

    Add day 10, part 2 tests

--- 2021/day10/day10_test.go
+++ 2021/day10/day10_test.go
@@ -26,26 +26,26 @@ func TestDay10Part1Test(t *testing.T) {
 	}
 }
 
-// func TestDay10Part2Example(t *testing.T) {
-// 	expected := 1134
-// 	answer, err := day10part2("./example.txt")
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+func TestDay10Part2Example(t *testing.T) {
+	expected := 288957
+	answer, err := day10part2("./example.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	if answer != expected {
-// 		t.Errorf("expected answer of %d, got %d", expected, answer)
-// 	}
-// }
+	if answer != expected {
+		t.Errorf("expected answer of %d, got %d", expected, answer)
+	}
+}
 
-// func TestDay10Part2Test(t *testing.T) {
-// 	expected := 1600104
-// 	answer, err := day10part2("./test.txt")
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+func TestDay10Part2Test(t *testing.T) {
+	expected := 2182912364
+	answer, err := day10part2("./test.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	if answer != expected {
-// 		t.Errorf("expected answer of %d, got %d", expected, answer)
-// 	}
-// }
+	if answer != expected {
+		t.Errorf("expected answer of %d, got %d", expected, answer)
+	}
+}
