e519836ef6d97d47d57239456a44348c1c3412b5
Riley Chase <riley.chase@rileychase.net>
2021-12-12 14:28:06 +1030

    Add day 9, part 1 tests

---
+++ 2021/day9/day9_test.go
@@ -0,0 +1,51 @@
+package day9
+
+import "testing"
+
+func TestDay9Part1Example(t *testing.T) {
+	expected := 15
+	answer, err := day9part1("./example.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if answer != expected {
+		t.Errorf("expected answer of %d, got %d", expected, answer)
+	}
+}
+
+func TestDay9Part1Test(t *testing.T) {
+	expected := 512
+	answer, err := day9part1("./test.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if answer != expected {
+		t.Errorf("expected answer of %d, got %d", expected, answer)
+	}
+}
+
+// func TestDay9Part2Example(t *testing.T) {
+// 	expected := 26
+// 	answer, err := day9part2("./example.txt")
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	if answer != expected {
+// 		t.Errorf("expected answer of %d, got %d", expected, answer)
+// 	}
+// }
+
+// func TestDay9Part2Test(t *testing.T) {
+// 	expected := 262
+// 	answer, err := day9part2("./test.txt")
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	if answer != expected {
+// 		t.Errorf("expected answer of %d, got %d", expected, answer)
+// 	}
+// }
