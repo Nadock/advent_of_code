88adc010aaee5e9cf22597cb7a9bfade526d84cc
Riley Chase <riley.chase@rileychase.net>
2022-12-03 15:57:47 +1030

    Add solution for day 3

---
+++ 2022/src/day3/day3.py
@@ -0,0 +1,39 @@
+import pathlib
+import string
+
+_priorities = dict(zip(string.ascii_letters, range(1, 53)))
+
+
+def item_priority(item: str) -> int:
+    return _priorities[item]
+
+
+def part1(path: pathlib.Path):
+    """Calculates the solution to day 3's first part."""
+    count = 0
+
+    for line in path.read_text("utf-8").split("\n"):
+        compartment1 = set(line[0 : len(line) // 2])
+        compartment2 = set(line[len(line) // 2 :])
+
+        for dupe in list(compartment1.intersection(compartment2)):
+            count += item_priority(dupe)
+
+    return count
+
+
+def part2(path: pathlib.Path):
+    """Calculates the solution to day 3's second part."""
+    lines = path.read_text("utf-8").strip().split("\n")
+    count = 0
+
+    for idx in range(0, len(lines), 3):
+        dupe = (
+            set(lines[idx])
+            .intersection(set(lines[idx + 1]))
+            .intersection(set(lines[idx + 2]))
+        )
+        assert len(dupe) == 1
+        count += item_priority(list(dupe)[0])
+
+    return count
