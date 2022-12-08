525b69193e22a763aecb7b4bcb58707d0f8cc49f
Riley Chase <riley.chase@rileychase.net>
2022-12-08 22:10:17 +1030

    Add solution for day 8

---
+++ 2022/src/day8/day8.py
@@ -0,0 +1,102 @@
+"""AOC Day 8 started at 2022-12-08T20:28:51.652941+10:30"""
+
+
+def part1(puzzle: str):
+    """Calculates the solution to day 8's first part."""
+    grid = []
+    for line in puzzle.splitlines():
+        grid.append([int(c) for c in line])
+
+    visible_trees = 0
+
+    for row, r in enumerate(grid):
+        for col, tree in enumerate(r):
+            visible = []
+            for i in range(0, row):
+                if grid[i][col] >= tree:
+                    visible.append(True)
+                    break
+            for i in range(row + 1, len(grid)):
+                if grid[i][col] >= tree:
+                    visible.append(True)
+                    break
+            for i in range(0, col):
+                if grid[row][i] >= tree:
+                    visible.append(True)
+                    break
+            for i in range(col + 1, len(grid[row])):
+                if grid[row][i] >= tree:
+                    visible.append(True)
+                    break
+
+            if len(visible) != 4:
+                visible_trees += 1
+
+    return visible_trees
+
+
+def part2(puzzle: str):
+    """Calculates the solution to day 8's second part."""
+    grid = []
+    for line in puzzle.splitlines():
+        grid.append([int(c) for c in line])
+
+    s_max = 0
+
+    for row, r in enumerate(grid):
+        for col, tree in enumerate(r):
+            s_sore = (
+                len(up(grid, row, col, tree))
+                * len(down(grid, row, col, tree))
+                * len(left(grid, row, col, tree))
+                * len(right(grid, row, col, tree))
+            )
+
+            if s_sore > s_max:
+                s_max = s_sore
+
+    return s_max
+
+
+def up(grid: list[list[int]], row: int, col: int, limit: int) -> list[int]:
+    if row == 0:
+        return []
+    l = []
+    for r in range(row - 1, -1, -1):
+        l.append(grid[r][col])
+        if grid[r][col] >= limit:
+            break
+    return l
+
+
+def down(grid: list[list[int]], row: int, col: int, limit: int) -> list[int]:
+    if row + 1 == len(grid):
+        return []
+    l = []
+    for r in range(row + 1, len(grid)):
+        l.append(grid[r][col])
+        if grid[r][col] >= limit:
+            break
+    return l
+
+
+def left(grid: list[list[int]], row: int, col: int, limit: int) -> list[int]:
+    if col == 0:
+        return []
+    l = []
+    for c in range(col - 1, -1, -1):
+        l.append(grid[row][c])
+        if grid[row][c] >= limit:
+            break
+    return l
+
+
+def right(grid: list[list[int]], row: int, col: int, limit: int) -> list[int]:
+    if col + 1 == len(grid[row]):
+        return []
+    l = []
+    for c in range(col + 1, len(grid[row])):
+        l.append(grid[row][c])
+        if grid[row][c] >= limit:
+            break
+    return l
