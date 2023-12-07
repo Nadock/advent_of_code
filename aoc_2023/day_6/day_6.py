0983911c9d75e9eaf1ad2d242f6d4da69c33f766
Riley Chase <riley.chase@rileychase.net>
2023-12-07 19:48:30 +1030

    Add AoC 2023 day 6 solution

--- aoc_2023/day_6/day_6.py
+++ aoc_2023/day_6/day_6.py
@@ -1,10 +1,40 @@
+from collections.abc import Generator
+
+
 def part_1(puzzle: str) -> int | str | float | bool:
     """Solution for AOC 2023, day 6, part 1."""
-    del puzzle
-    return "Part 1 TBD"
+    times = [int(t) for t in puzzle.splitlines()[0].split("Time: ", 1)[1].split()]
+    dists = [int(d) for d in puzzle.splitlines()[1].split("Distance: ", 1)[1].split()]
+
+    multi = 1
+
+    for time, dist in zip(times, dists, strict=True):
+        count = 0
+        for v, t in iter_v_t(time):
+            d = v * t
+            if d > dist:
+                count += 1
+        multi *= count
+
+    return multi
+
+
+def iter_v_t(time: int) -> Generator[tuple[int, int], None, None]:
+    for i in range(time):
+        v = time - i
+        t = time - v
+        yield v, t
 
 
 def part_2(puzzle: str) -> int | str | float | bool:
     """Solution for AOC 2023, day 6, part 2."""
-    del puzzle
-    return "Part 2 TBD"
+    time = int("".join(puzzle.splitlines()[0].split("Time: ", 1)[1].split()))
+    dist = int("".join(puzzle.splitlines()[1].split("Distance: ", 1)[1].split()))
+
+    count = 0
+    for v, t in iter_v_t(time):
+        d = v * t
+        if d > dist:
+            count += 1
+
+    return count
