9f1bc331be22e13b200a463bc2341bf81ae03df5
Riley Chase <riley.chase@rileychase.net>
2024-12-01 21:59:23 +1030

    Add AoC 2024 day 1, part 1 & 2 solutions

--- aoc_2024/day_1/day_1.py
+++ aoc_2024/day_1/day_1.py
@@ -1,10 +1,34 @@
 def part_1(puzzle: str) -> int | str | float | bool:
     """Solution for AOC 2024, day 1, part 1."""
-    del puzzle
-    return "Part 1 TBD"
+    left, right = [], []
+    for line in puzzle.splitlines():
+        left.append(int(line.split()[0]))
+        right.append(int(line.split()[1]))
+
+    left.sort()
+    right.sort()
+
+    sum = 0
+    for l, r in zip(left, right, strict=True):
+        sum += abs(l - r)
+
+    return sum
 
 
 def part_2(puzzle: str) -> int | str | float | bool:
     """Solution for AOC 2024, day 1, part 2."""
-    del puzzle
-    return "Part 2 TBD"
+    left, right = [], []
+    for line in puzzle.splitlines():
+        left.append(int(line.split()[0]))
+        right.append(int(line.split()[1]))
+
+    right_counts: dict[int, int] = {}
+    for r in right:
+        right_counts.setdefault(r, 0)
+        right_counts[r] += 1
+
+    sum = 0
+    for l in left:
+        sum += l * right_counts.get(l, 0)
+
+    return sum
