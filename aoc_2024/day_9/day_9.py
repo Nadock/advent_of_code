e29ed78ff7905f7e9354d8af4b2d7da890140533
Riley Chase <riley.chase@rileychase.net>
2024-12-12 20:07:18 +1100

    Add generated files for AOC 2024 day 9

---
+++ aoc_2024/day_9/day_9.py
@@ -0,0 +1,10 @@
+def part_1(puzzle: str) -> int | str | float | bool:
+    """Solution for AOC 2024, day 9, part 1."""
+    del puzzle
+    return "Part 1 TBD"
+
+
+def part_2(puzzle: str) -> int | str | float | bool:
+    """Solution for AOC 2024, day 9, part 2."""
+    del puzzle
+    return "Part 2 TBD"
