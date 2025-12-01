fb9af02519e8aba493efb89ef0703b31712df964
Riley Chase <riley.chase@rileychase.net>
2025-12-01 15:52:43 +1030

    Add AoC 2025, day 1 solutions

--- aoc_2025/day_1/day_1.py
+++ aoc_2025/day_1/day_1.py
@@ -1,10 +1,40 @@
 def part_1(puzzle: str) -> int | str | float | bool:
     """Solution for AOC 2025, day 1, part 1."""
-    del puzzle
-    return "Part 1 TBD"
+    dial = 50
+    count = 0
+
+    for line in puzzle.splitlines():
+        dir = line.strip()[0]
+        amount = int(line.strip()[1:]) % 100
+
+        if dir == "R":
+            dial = (dial + amount) % 100
+        elif dir == "L":
+            dial = (dial - amount) % 100
+
+        if dial == 0:
+            count += 1
+
+    return count
 
 
 def part_2(puzzle: str) -> int | str | float | bool:
     """Solution for AOC 2025, day 1, part 2."""
-    del puzzle
-    return "Part 2 TBD"
+    dial = 50
+    count = 0
+
+    for line in puzzle.splitlines():
+        dir = line.strip()[0]
+        amount = int(line.strip()[1:]) % 100
+        full_rot = int(line.strip()[1:]) // 100
+
+        if dir == "R":
+            full_rot += (dial + amount) // 100
+            dial = (dial + amount) % 100
+        elif dir == "L":
+            full_rot -= (dial - amount) // 100
+            dial = (dial - amount) % 100
+
+        count += full_rot
+
+    return count
