2e9780942a362898f9786edfb7229b4578e9850e
Riley Chase <riley.chase@rileychase.net>
2025-12-03 21:49:35 +1030

    Add AoC 2025 day 3 part 1 solution

--- aoc_2025/day_3/day_3.py
+++ aoc_2025/day_3/day_3.py
@@ -1,7 +1,23 @@
 def part_1(puzzle: str) -> int | str | float | bool:
     """Solution for AOC 2025, day 3, part 1."""
-    del puzzle
-    return "Part 1 TBD"
+    banks = puzzle.splitlines()
+    total = 0
+
+    for bank in banks:
+        max = int(bank[:2])
+
+        for curr in range(len(bank)):
+            if int(bank[curr]) < int(str(max)[0]):
+                continue
+
+            for next in range(curr + 1, len(bank)):
+                value = int(bank[curr] + bank[next])
+                if value > max:  # noqa: PLR1730
+                    max = value
+
+        total += max
+
+    return total
 
 
 def part_2(puzzle: str) -> int | str | float | bool:
